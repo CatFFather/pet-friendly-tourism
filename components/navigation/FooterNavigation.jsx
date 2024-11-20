ae0c79bcd181219af83af068aecc1cb9d94eabd7
이재홍 <jh2000lee@nate.com>
2024-11-20 11:48:43 +0900

    [ADD]NavigationIcon 추가 --> use client 사용 때문에 컴포넌트 화

--- components/navigation/FooterNavigation.jsx
+++ components/navigation/FooterNavigation.jsx
@@ -1,36 +1,29 @@
 import Link from "next/link";
-import {
-  HomeIcon,
-  UserIcon,
-  MagnifyingGlassIcon,
-} from "@heroicons/react/24/outline";
+// COMPONENT
+import NavigationIcon from "@/components/navigation/NavigationIcon";
 
-// CLASS
-const iconClassName = "h-6 w-6 text-[#4ED1AD]";
-const linkClassName = "flex flex-col items-center text-xs";
-// TODO 나중에 Link list를 만들어서 정의 할 수있으면 하면 좋을듯 일단 로그인 페이지로 연결해놨음
+const navItems = [
+  { href: "/home", label: "홈" },
+  { href: "/search", label: "검색" },
+  { href: "/login", label: "회원" },
+];
+
 // 하단 버튼
 export default function FooterNavigation({}) {
   return (
     <ul className="flex justify-between bg-[#FFFFFF] h-14 items-center px-4 fixed bottom-0 w-[inherit] border-t">
-      <li>
-        <Link href="/home" className={linkClassName}>
-          <HomeIcon className={iconClassName} />
-          <span>홈</span>
-        </Link>
-      </li>
-      <li>
-        <Link href="/search" className={linkClassName}>
-          <MagnifyingGlassIcon className={iconClassName} />
-          <span>검색</span>
-        </Link>
-      </li>
-      <li>
-        <Link href="/login" className={linkClassName}>
-          <UserIcon className={iconClassName} />
-          <span>회원</span>
-        </Link>
-      </li>
+      {navItems.map((nav) => {
+        return (
+          <li key={nav.href}>
+            <Link
+              href={nav.href}
+              className="flex flex-col items-center text-xs"
+            >
+              <NavigationIcon nav={nav} href={nav.href} />
+            </Link>
+          </li>
+        );
+      })}
     </ul>
   );
 }
---
+++ components/navigation/NavigationIcon.jsx
@@ -0,0 +1,37 @@
+"use client";
+import { usePathname } from "next/navigation";
+// ICON
+import {
+  HomeIcon,
+  MagnifyingGlassIcon,
+  UserIcon,
+} from "@heroicons/react/24/outline";
+
+/**
+ * 네비 아이콘
+ * 컴포넌트로 따로 빼지 않아도 되는데 use client 선언되는 부분을 최소화 하기 위해 컴포넌트로 빼서 사용(FooterNavigation에서 그냥 usePathname 사용 해도 됨)
+ * @param {*} nav
+ * @returns
+ */
+export default function NavigationIcon({ nav }) {
+  const pathname = usePathname();
+  const iconClassName = `h-6 w-6`;
+  const textClassName = `text-[${
+    nav.href == pathname ? "#4ED1AD" : "#B6BDBE"
+  }]`;
+
+  return (
+    <>
+      {nav.href == "/home" && (
+        <HomeIcon className={`${iconClassName} ${textClassName}`} />
+      )}
+      {nav.href == "/search" && (
+        <MagnifyingGlassIcon className={`${iconClassName} ${textClassName}`} />
+      )}
+      {nav.href == "/login" && (
+        <UserIcon className={`${iconClassName} ${textClassName}`} />
+      )}
+      <span className={textClassName}>{nav.label}</span>
+    </>
+  );
+}
