a5a696d1165cea5d3f935c507fa2a0722f58e500
unknown <jh2000lee@nate.com>
2024-11-24 20:22:36 +0700

    [MOD]useObserver으로 무한스크롤 적용

--- app/(main)/home/(list)/HomeList.jsx
+++ app/(main)/home/(list)/HomeList.jsx
@@ -1,16 +1,56 @@
 'use client';
+import { useEffect, useState } from 'react';
 import Image from 'next/image';
 // SERVICE
 import KorPetTourService from '@/service/KorPetTourService ';
-import { useEffect, useState } from 'react';
-
+import useObserver from '@/hooks/useObserver';
 export default function HomeList({ initialData }) {
+  const listLoadingRef = useObserver({
+    callback: setNextPage,
+  });
   const [list, setList] = useState([
     ...initialData?.response?.body?.items?.item,
   ]);
-  useEffect(() => {
-    console.log('list', list);
-  }, [list]);
+  const [pagination, setPagination] = useState({
+    numOfRows: initialData?.response?.body?.numOfRows,
+    pageNo: initialData?.response?.body?.pageNo,
+    totalCount: initialData?.response?.body?.totalCount,
+  });
+  const [listLoading, setListLoading] = useState(false);
+
+  // useEffect(() => {
+  //   console.log('list', list);
+  // }, [list]);
+  // useEffect(() => {
+  //   console.log('pagination', pagination);
+  // }, [pagination]);
+
+  // 다음 페이지 불러오기
+  function setNextPage() {
+    if (listLoading) return;
+    if (list?.length >= pagination?.totalCount) return;
+    else getAreaBasedList(pagination.pageNo + 1);
+  }
+
+  // 지역기반 관광정보조회
+  function getAreaBasedList(pageNo) {
+    setListLoading(true);
+    KorPetTourService.getAreaBasedList({
+      numOfRows: pagination.numOfRows,
+      pageNo,
+    })
+      .then((res) => {
+        // console.log('res', res);
+        setList([...list, ...res?.data.response?.body?.items?.item]);
+        setPagination({
+          numOfRows: res?.data?.response?.body?.numOfRows,
+          pageNo: res?.data?.response?.body?.pageNo,
+          totalCount: res?.data?.response?.body?.totalCount,
+        });
+      })
+      .catch((e) => console.log('e', e))
+      .finally(() => setListLoading(false));
+  }
 
   return (
     <div className="flex flex-wrap gap-2 pt-5 pb-10 px-4">
@@ -36,6 +76,9 @@ export default function HomeList({ initialData }) {
           </div>
         );
       })}
+      {!listLoading && list?.length < pagination?.totalCount && (
+        <div ref={listLoadingRef.setRef}>다음 페이지@@@@@@@@@@@</div>
+      )}
     </div>
   );
 }
