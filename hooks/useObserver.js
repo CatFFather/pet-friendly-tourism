e859be60c1bbccd1ddc54022877b4cdf3f00421f
unknown <jh2000lee@nate.com>
2024-11-24 20:22:15 +0700

    [ADD]useObserver hook 추가

---
+++ hooks/useObserver.js
@@ -0,0 +1,26 @@
+import { useState, useEffect } from 'react';
+
+export default function useObserver({
+  callback, // 감지 시 실행할 callback 함수
+  threshold = 1.0, // ref가 100% 보여질 때 callback이 실행 (0.1 ~ 1 사이)
+}) {
+  const [ref, setRef] = useState(null);
+
+  function onIntersect([entry], observer) {
+    if (entry.isIntersecting) {
+      observer.unobserve(entry.target);
+      callback();
+      observer.observe(entry.target);
+    }
+  }
+  useEffect(() => {
+    let observer;
+    if (ref) {
+      observer = new IntersectionObserver(onIntersect, { threshold });
+      observer.observe(ref);
+    }
+    return () => observer && observer.disconnect();
+  }, [ref]);
+
+  return { ref, setRef };
+}
