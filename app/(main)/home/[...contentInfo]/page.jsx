e797177bed2a5364d83caac28f191563b6274f32
이재홍 <jh2000lee@nate.com>
2024-11-26 13:54:05 +0900

    [MOD]상세 데이터 가져와서 제목, 내용, 이미지 적용

--- app/(main)/home/[...contentInfo]/page.jsx
+++ app/(main)/home/[...contentInfo]/page.jsx
@@ -1,127 +1,66 @@
-// 'use client';
-import axios from 'axios';
-import qs from 'qs';
-
-// import { useRouter } from 'next/navigation';
-// import { useEffect, use } from 'react';
-
+import Image from 'next/image';
 // SERVICE
 import KorPetTourService from '@/service/KorPetTourService ';
 // COMPONENT
-import HomeList from '@/app/(main)/home/(list)/HomeList';
 
 // 상세 데이터 가져오기
 async function getDetailItems(contentTypeId, contentId) {
-  console.log('contentTypeId ', contentTypeId);
-  console.log('contentId', contentId);
-
-  const params = {
-    serviceKey: process.env.NEXT_PUBLIC_KOR_PET_TOUR_SERVICE_API_KEY,
-    MobileApp: 'test-app', // TODO 나중에 변경
-    MobileOS: 'WIN', // TODO 모바일, 웹 구분 필요
-    _type: 'json',
-    contentTypeId,
-    contentId,
+  const [detailPetTour, detailInfo, detailCommon, detailIntro] =
+    await Promise.all([
+      KorPetTourService.getDetailPetTour({
+        contentId,
+      }),
+      KorPetTourService.getDetailInfo({
+        contentTypeId,
+        contentId,
+      }),
+      KorPetTourService.getDetailCommon({
+        contentId,
+        defaultYN: 'Y',
+        firstImageYN: 'Y',
+        areacodeYN: 'Y',
+        catcodeYN: 'Y',
+        addrinfoYN: 'Y',
+        mapinfoYN: 'Y',
+        overviewYN: 'Y',
+      }),
+      KorPetTourService.getDetailIntro({
+        contentTypeId,
+        contentId,
+      }),
+    ]);
+  const detailItems = {
+    petTour: detailPetTour?.response?.body?.items?.item?.[0],
+    info: detailInfo?.response?.body?.items?.item?.[0],
+    common: detailCommon?.response?.body?.items?.item?.[0],
+    intro: detailIntro?.response?.body?.items?.item?.[0],
   };
-  console.log('params', params);
-  console.log('qs.stringify(params)', qs.stringify(params));
-  const url = `${
-    process.env.NEXT_PUBLIC_KOR_PET_TOUR_SERVICE_API_URL
-  }/detailInfo?${qs.stringify(params)}`;
-  console.log('url', url);
-  const response = await fetch(url);
-  console.log('response', response);
-  const data = await response.json();
-  console.log('data', data);
-  return data;
-
-  // try {
-  //   const { data } = await KorPetTourService.getDetailCommon({
-  //     contentTypeId,
-  //     contentId,
-  //     // numOfRows: 50,
-  //   });
-  //   console.log('data', data);
-  //   return data;
-  // } catch (e) {
-  //   console.log('e', e);
-  // }
+  return detailItems;
 }
 
 export default async function ContentPage({ params }) {
-  // const { contentInfo } = use(params);
-  // console.log('contentInfo', contentInfo);
-  // // 상세 데이터 가져오기
-  // async function getDetailItems(contentTypeId, contentId) {
-  //   console.log('contentTypeId ', contentTypeId);
-  //   console.log('contentId', contentId);
-
-  //   const url =
-  //     'https://apis.data.go.kr/B551011/KorPetTourService/detailInfo?serviceKey=CiigqLJetyGEPTWT3WrAEexRWR82paaea%2FqNNVRcwtZXCE%2Fq2R3YnCdR2WGzr%2FIjF%2BOzsQon5Ncfb06PV8CVhQ%3D%3D&MobileOS=WIN&MobileApp=test-app&_type=json&contentId=2376711&contentTypeId=32';
-  //   // fetch 로 데이터 가져오기
-  //   // const response = await fetch(url, {
-  //   //   // headers: {
-  //   //   //   'Content-Type': 'application/json',
-  //   //   //   'API-Key': process.env.DATA_API_KEY,
-  //   //   // },
-  //   // });
-  //   // console.log('response', response);
-  //   // const data = await response.json();
-  //   // console.log('data', data);
-
-  //   try {
-  //     const data = await KorPetTourService.getDetailData({
-  //       contentTypeId,
-  //       contentId,
-  //       // numOfRows: 1,
-  //       // pageNo: 1,
-  //       // numOfRows: 50,
-  //     });
-  //     console.log('data??????????', data);
-  //     return data;
-  //   } catch (e) {
-  //     console.log('e', e);
-  //   }
-
-  //   // try {
-  //   //   const data = await KorPetTourService.getDetailInfo({
-  //   //     contentTypeId: '2376711',
-  //   //     contentId: '32',
-  //   //     // numOfRows: 1,
-  //   //     // pageNo: 1,
-  //   //     // numOfRows: 50,
-  //   //   });
-  //   //   console.log('data??????????', data);
-  //   //   return data;
-  //   // } catch (e) {
-  //   //   console.log('e', e);
-  //   // }
-  //   // try {
-  //   //   const url =
-  //   //     'https://apis.data.go.kr/B551011/KorPetTourService/detailInfo?serviceKey=CiigqLJetyGEPTWT3WrAEexRWR82paaea%2FqNNVRcwtZXCE%2Fq2R3YnCdR2WGzr%2FIjF%2BOzsQon5Ncfb06PV8CVhQ%3D%3D&MobileOS=WIN&MobileApp=test-app&_type=json&contentId=2376711&contentTypeId=32';
-  //   //   const response = await axios.get(url, {
-  //   //     headers: {},
-  //   //   });
-  //   //   console.log('response', response);
-  //   // } catch (err) {
-  //   //   console.log('err', err);
-  //   // }
-  // }
-  // useEffect(() => {
-  //   getDetailItems(contentInfo[0], contentInfo[1]);
-  // }, []);
-
-  // const router = useRouter();
-  // const [title, id] = router?.query?.params;
-  // useEffect(() => {
-  //   console.log('title', title);
-  // }, []);
-
   const { contentInfo } = await params;
   const contentTypeId = contentInfo?.[0];
   const contentId = contentInfo?.[1];
-  console.log('contentInfo', contentInfo);
   const detailItems = await getDetailItems(contentTypeId, contentId); // 서버에서 데이터 가져오기
+  const { petTour, info, common, intro } = detailItems;
   console.log('detailItems', detailItems);
-  return <>상세@@@@@@@@@@@@@@@@@@@@@@@</>;
+  return (
+    <div>
+      <div>{common.title}</div>
+      <div className="relative after:content-[''] after:pb-[90%] after:block">
+        <Image
+          fill
+          className="object-cover rounded-lg box-border border border-[#F3F6F6]"
+          src={
+            common?.firstimage ||
+            common?.firstimage2 ||
+            '/images/DefaultImage.webp'
+          }
+          alt={common?.title}
+        />
+      </div>
+      <div>{common.overview}</div>
+    </div>
+  );
 }
