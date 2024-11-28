7431a26786b799875015696bb410ebecc7694513
이재홍 <jh2000lee@nate.com>
2024-11-28 14:52:49 +0900

    [MOD]BackButton 적용

--- components/layout/TopBackButtonLayout.jsx
+++ components/layout/TopBackButtonLayout.jsx
@@ -1,15 +1,9 @@
-'use client';
-import { useRouter } from 'next/navigation';
-import { ArrowLeftIcon } from '@heroicons/react/24/outline';
-
+import BackButton from '@/components/button/BackButton';
 export default function TopBackButtonLayout({ children, tilte }) {
-  const router = useRouter();
   return (
     <div className="relative">
       <div className="sticky top-0 bg-[#FFFFFF] z-50 px-4 h-11 flex items-center">
-        <button onClick={() => router.back()}>
-          <ArrowLeftIcon className="h-10 w-10 p-2 -ml-2" />
-        </button>
+        <BackButton />
         <span className="font-semibold text-[#000000] text-lg mt-0.5 flex-1 truncate">
           {tilte}
         </span>
