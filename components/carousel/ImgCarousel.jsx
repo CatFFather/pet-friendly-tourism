e495288a9d22fe8b7b01fdb09e8d47e5d47950cd
unknown <jh2000lee@nate.com>
2024-12-08 16:09:34 +0700

    [MOD]onMouseMove, onMouseUp 함수를 onMouseDown 실행 시 전역 이벤트로 등록

--- components/carousel/ImgCarousel.jsx
+++ components/carousel/ImgCarousel.jsx
@@ -5,17 +5,23 @@ import Image from 'next/image';
 import { ChevronRightIcon, ChevronLeftIcon } from '@heroicons/react/24/outline';
 
 export default function ImgCarousel({ images }) {
+  // 0번 index에는 마지막 아이템이 하나 더 들어있고 마지막 index에는 첫 번째 아이템이 하나 더 넣어줌 --> 무한캐러셀 구현을 위함
   const totalImages = [
     images?.[images?.length - 1],
     ...(images || []),
     images?.[0],
-  ]; // 0번 index에는 마지막 아이템이 하나 더 들어있고 마지막 index에는 첫 번째 아이템이 하나 더 넣어줌 --> 무한캐러셀 구현을 위함
-  const imgWrapRef = useRef(null);
-  const [currentIndex, setCurrentIndex] = useState(1);
+  ];
+  const imgWrapRef = useRef(null); // 사진 영역
+  const isDragging = useRef(false); // 드래그 중 여부
+  const startPosition = useRef(0); // 사진 드래그 시작 x축 시작 포인트
+  const [currentIndex, setCurrentIndex] = useState(1); // 현재 보이는 사진의 index
 
-  const isDragging = useRef(false);
-  const startPosition = useRef(0);
-  const [endPosition, setEndPosition] = useState(0);
+  useEffect(() => {
+    return () => {
+      window.removeEventListener('mousemove', onMouseMove);
+      window.removeEventListener('mouseup', onMouseUp);
+    };
+  }, []);
 
   useEffect(() => {
     movingImgWrap();
@@ -66,28 +72,24 @@ export default function ImgCarousel({ images }) {
   }
   // 마우스 클릭 했을 때
   function onMouseDown(e) {
-    e.preventDefault();
-    console.log('onMouseDown################', e);
     isDragging.current = true;
     startPosition.current = e.pageX;
+    // imgWrapRef에 onMouseMove, onMouseUp를 직접 선언하면 마우스 이벤트가 imgWrapRef의 영역 밖으로 이동했을 때 onMouseUp 이벤트가 발생하지 않게됨
+    // --> 전역 이벤트 리스너 추가하여 이벤트 추적
+    // 전역 이벤트 리스너 추가
+    window.addEventListener('mousemove', onMouseMove);
+    window.addEventListener('mouseup', onMouseUp);
   }
   // 마우스 움직임 감지
   function onMouseMove(e) {
     if (!isDragging.current) return;
-    console.log('onMouseMove@@@@@@@@@@@@@', e);
-    console.log('imgWrapRef', imgWrapRef);
-    console.log('startPosition.current', startPosition.current);
     const currentPosition = e?.pageX;
-    console.log('currentPosition', currentPosition);
     const difference = startPosition.current - currentPosition;
     const absolute = Math.abs(difference);
-    console.log('difference', difference);
-    console.log('absolute', absolute);
     const operator = difference > 0 ? '-' : '+'; // 차이가 0보다 크면 다음 작으면 이전 --> operator는 -일 때 다음 사진임
     const movingWidth = `calc(-${
       currentIndex * 100
     }% ${operator} ${absolute}px)`;
-    console.log('movingWidth', movingWidth);
     imgWrapRef.current?.style?.setProperty('transition-duration', `0ms`); // duration을 0초로 초기화 후 이동
     imgWrapRef.current?.style?.setProperty(
       'transform',
@@ -96,12 +98,14 @@ export default function ImgCarousel({ images }) {
   }
   // 마우스 클릭을 끝냈을 때
   function onMouseUp(e) {
-    console.log('onMouseUp!!!!!!!!!!', e);
     isDragging.current = false;
     const currentPosition = e.pageX;
     const difference = startPosition.current - currentPosition;
     const absolute = Math.abs(difference);
     const type = difference > 0 ? 'next' : 'prev'; // 차이가 0보다 크면 다음 작으면 이전
+    // 전역 이벤트 리스너 제거
+    window.removeEventListener('mousemove', onMouseMove);
+    window.removeEventListener('mouseup', onMouseUp);
     // 100만큼 이동하지 못하면 원래 위치로 이동
     if (absolute < 100) {
       imgWrapRef.current.style.setProperty('transition-duration', `500ms`);
@@ -119,8 +123,6 @@ export default function ImgCarousel({ images }) {
           // onTouchStart
           // onTouchEnd
           onMouseDown={onMouseDown}
-          onMouseMove={onMouseMove}
-          onMouseUp={onMouseUp}
           className="flex transition-transform translate-x-[-100%]"
         >
           {totalImages?.map((image, index) => {
