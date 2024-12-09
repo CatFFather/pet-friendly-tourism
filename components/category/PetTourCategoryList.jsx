72770c52d0a22e6f4bd387ee7788cb438d8da1d7
이재홍 <jh2000lee@nate.com>
2024-12-09 11:19:21 +0900

    [MOD]빌드시 애러 발생--> useSearchParams() 사용 시 Suspense 필수로 감싸줘야함

--- components/category/PetTourCategoryList.jsx
+++ components/category/PetTourCategoryList.jsx
@@ -1,5 +1,5 @@
 'use client';
-import { useEffect, useRef } from 'react';
+import { useEffect, Suspense, useState } from 'react';
 import { usePathname, useSearchParams } from 'next/navigation';
 import Link from 'next/link';
 
@@ -8,62 +8,68 @@ export default function PetTourCategoryList({ categoryCodeList }) {
   const pathname = usePathname();
   const searchParams = useSearchParams();
   const cat1 = searchParams.get('cat1') || ''; // 예외처리 하여 기본값 '' 로 변경(전체일 때)
-  const categoryListRef = useRef(null);
-  const markerRef = useRef(null);
+
+  // ref를 state로 전환
+  // --> Suspense가 추가 되면서 처음 실행 시 categoryListRef,markerRef가 랜더링 전에 useEffect 가 실행 되어 초기 스타일을 지정(setProperty) 해주지 못함
+  const [categoryListRef, setCategoryListRef] = useState(null);
+  const [markerRef, setMarkerRef] = useState(null);
 
   useEffect(() => {
-    const menus = categoryListRef?.current?.querySelectorAll('.category__code');
+    if (!categoryListRef || !markerRef) return;
+    const menus = categoryListRef?.querySelectorAll('.category__code') || [];
     const findIndexMenus = [...menus]?.findIndex((menu) => menu?.id == cat1);
     const movingTodalWidth = [...menus]?.reduce((acc, data, index) => {
       if (index >= findIndexMenus) return acc;
       return acc + data?.offsetWidth;
     }, 0);
     const currentMenuWidth = menus?.[findIndexMenus]?.offsetWidth;
-    markerRef.current.style.setProperty('width', `${currentMenuWidth}px`);
-    markerRef.current.style.setProperty(
+    markerRef?.style?.setProperty('width', `${currentMenuWidth}px`);
+    markerRef?.style?.setProperty(
       'transform',
       `translateX(${movingTodalWidth}px)`,
     );
-    categoryListRef?.current?.scrollTo({
+    categoryListRef?.scrollTo({
       top: 0,
       left: movingTodalWidth,
       behavior: 'smooth',
     });
-  }, [cat1]);
+  }, [cat1, categoryListRef, markerRef]);
 
   return (
-    <ul
-      ref={categoryListRef}
-      className="px-1.5 overflow-x-auto whitespace-nowrap [&::-webkit-scrollbar]:hidden border-b"
-    >
-      <div className="flex items-center gap relative">
-        {categoryCodeList?.map((code) => {
-          return (
-            <Link
-              key={code?.code}
-              href={{ pathname, query: code?.code && { cat1: code?.code } }}
-              replace
-            >
-              <li
-                id={code?.code}
-                className="category__code px-2.5 pt-3.5 pb-2.5"
+    <Suspense>
+      <ul
+        ref={setCategoryListRef}
+        className="px-1.5 overflow-x-auto whitespace-nowrap [&::-webkit-scrollbar]:hidden border-b"
+      >
+        <div className="flex items-center gap relative">
+          {categoryCodeList?.map((code) => {
+            return (
+              <Link
+                key={code?.code}
+                href={{ pathname, query: code?.code && { cat1: code?.code } }}
+                replace
               >
-                <span
-                  className={`text-sm text-[#2D3035] ${
-                    cat1 == code?.code ? 'font-bold' : 'font-normal'
-                  }`}
+                <li
+                  id={code?.code}
+                  className="category__code px-2.5 pt-3.5 pb-2.5"
                 >
-                  {code?.name}
-                </span>
-              </li>
-            </Link>
-          );
-        })}
-        <div
-          ref={markerRef}
-          className="absolute bottom-0 h-1 bg-[#FF4081] transition-transform duration-300"
-        />
-      </div>
-    </ul>
+                  <span
+                    className={`text-sm text-[#2D3035] ${
+                      cat1 == code?.code ? 'font-bold' : 'font-normal'
+                    }`}
+                  >
+                    {code?.name}
+                  </span>
+                </li>
+              </Link>
+            );
+          })}
+          <div
+            ref={setMarkerRef}
+            className="absolute bottom-0 h-1 bg-[#FF4081] transition-transform duration-300"
+          />
+        </div>
+      </ul>
+    </Suspense>
   );
 }
