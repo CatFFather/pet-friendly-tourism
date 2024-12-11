8a14b5d91d19de27caa18a2a01b43eb20f0c20bb
이재홍 <jh2000lee@nate.com>
2024-12-11 17:37:36 +0900

    [MOD]좌측 화면 input 이용하여 검색 기능 추가

--- components/container/RootContainer.jsx
+++ components/container/RootContainer.jsx
@@ -1,10 +1,23 @@
+import Form from 'next/form';
+// COMPONENT
+import SearchInput from '@/components/input/SearchInput';
+
 export default function RootContainer({ children }) {
   return (
-    <div className="flex justify-center">
+    <div className="flex justify-center gap-5">
       {/* 좌측화면 1024px까지만 노출 */}
-      <div className="flex flex-col  sticky top-0 h-screen items-center justify-center">
+      <div className="flex flex-col sticky top-0 h-screen max-w-[400px] py-10 gap-4 justify-between">
         <div>개모임</div>
-        <div>검색 input</div>
+        <div className="flex flex-col gap-3.5">
+          <p>
+            개모임은 반려동물 동반여행 가능한 관광지, 문화시설, 축제공연행사,
+            숙박, 음식점, 레포츠, 쇼핑의 관광정보를 제공합니다.
+          </p>
+          <Form action="/search">
+            <SearchInput name="keyword" />
+          </Form>
+        </div>
+        <div></div>
       </div>
       {/* 우측화면 실제 모바일 화면 */}
       <div className="min-h-screen w-[430px] relative border-x border-[#E1E1E1] border-solid box-content">
