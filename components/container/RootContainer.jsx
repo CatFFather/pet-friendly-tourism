69b6ea5e4ccd73dfcf12329df8fbaacd311649cc
이재홍 <jh2000lee@nate.com>
2024-12-11 18:27:38 +0900

    [MOD]1024px 기준으로 왼쪽 화면 사라지고 640px 기준으로 full

--- components/container/RootContainer.jsx
+++ components/container/RootContainer.jsx
@@ -6,7 +6,7 @@ export default function RootContainer({ children }) {
   return (
     <div className="flex justify-center gap-5">
       {/* 좌측화면 1024px까지만 노출 */}
-      <div className="flex flex-col sticky top-0 h-screen max-w-[400px] py-10 gap-4 justify-between">
+      <div className="flex flex-col sticky top-0 h-screen max-w-[400px] py-10 gap-4 justify-between max-lg:hidden">
         <div>개모임</div>
         <div className="flex flex-col gap-3.5">
           <p>
@@ -19,8 +19,8 @@ export default function RootContainer({ children }) {
         </div>
         <div></div>
       </div>
-      {/* 우측화면 실제 모바일 화면 */}
-      <div className="min-h-screen w-[430px] relative border-x border-[#E1E1E1] border-solid box-content">
+      {/* 우측화면 실제 모바일 화면 640px 기준으로 full */}
+      <div className="min-h-screen w-[430px] relative border-x border-[#E1E1E1] border-solid box-content max-sm:w-full">
         {children}
       </div>
     </div>
