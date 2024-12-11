f8c5116087282795b02e17c2c02829abe66a736d
이재홍 <jh2000lee@nate.com>
2024-12-11 15:29:17 +0900

    [MOD]외부에서 들어왔을 때 검색 defaultValue 넣어주기

--- app/(footer-navigation)/search/layout.jsx
+++ app/(footer-navigation)/search/layout.jsx
@@ -1,16 +1,19 @@
+'use client';
 import Form from 'next/form';
+import { useSearchParams } from 'next/navigation';
 
 // COMPONENT
 import SearchInput from '@/components/input/SearchInput';
 
-export default async function SearchLayout({ children }) {
-  // TODO 만약 외부에서 qs keyword를 넣어 진입 했을 때 defaultValue를 qs로 넣어줄 수 있을지 고민
+export default function SearchLayout({ children }) {
+  const searchParams = useSearchParams();
+  const keyword = searchParams.get('keyword');
   return (
     <div className="relative">
       <div className="sticky top-0 bg-[#FFFFFF] z-50">
         <div className="px-1.5 py-2">
           <Form action="">
-            <SearchInput name="keyword" />
+            <SearchInput name="keyword" defaultValue={keyword} />
           </Form>
         </div>
       </div>
