20e011a46071478cff03821b69b6a04696868f2c
이재홍 <jh2000lee@nate.com>
2024-12-13 17:18:16 +0900

    [MOD]최근 검색 내역 삭제, 최근 검색어 10개 제한, Empty 적용, css 적용

--- app/(footer-navigation)/search/(list)/SearchList.jsx
+++ app/(footer-navigation)/search/(list)/SearchList.jsx
@@ -3,8 +3,10 @@
 import { Fragment, useEffect, useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
+import { XMarkIcon } from '@heroicons/react/24/outline';
 // COMPONENT
 import ImgSkeleton from '@/components/card/ImgSkeleton';
+import Empty from '@/components/common/Empty';
 // SERVICE
 import KorPetTourService from '@/service/KorPetTourService ';
 import useObserver from '@/hooks/useObserver';
@@ -21,16 +23,10 @@ export default function SearchList({ initialData, query }) {
   const [keywordList, setKeywordList] = useState(null); // 키워드 리스트
 
   useEffect(() => {
-    console.log('searchList', searchList);
-    console.log('keywordList', keywordList);
-  }, [searchList, keywordList]);
-  useEffect(() => {
     const recentKeywordList = JSON.parse(
       LocalStorage?.getItem('recent-keyword-list'),
     );
-    console.log('recentKeywordList', recentKeywordList);
     if (!query?.keyword) return setKeywordList(recentKeywordList);
-    console.log('통과@@@@@@@@@@@@');
     const filterList =
       recentKeywordList?.filter(
         (keyword) => keyword?.value != query?.keyword,
@@ -39,7 +35,8 @@ export default function SearchList({ initialData, query }) {
       key: filterList?.[0]?.key + 1 || 1,
       value: query?.keyword,
     };
-    const newList = [newKeyword, ...filterList] || [];
+    const newList =
+      [newKeyword, ...filterList].filter((data, index) => index < 10) || [];
     LocalStorage?.setItem('recent-keyword-list', JSON.stringify(newList));
     // 최근 검색어 저장
     setKeywordList(newList);
@@ -84,50 +81,109 @@ export default function SearchList({ initialData, query }) {
       .catch((e) => console.log('e', e))
       .finally(() => setListLoading(false));
   }
+  // 최근 검색 내역 삭제
+  function deleteRecentKeywordList(key) {
+    if (key == 'ALL') {
+      setKeywordList([]);
+      LocalStorage?.setItem('recent-keyword-list', JSON.stringify([]));
+    } else {
+      const newKeywordList = keywordList.filter(
+        (keyword) => keyword.key != key,
+      );
+      setKeywordList(newKeywordList);
+      LocalStorage?.setItem(
+        'recent-keyword-list',
+        JSON.stringify(newKeywordList),
+      );
+    }
+  }
+
   return (
     <div className="flex flex-wrap gap-2 pt-5 pb-10 px-4">
       {!query?.keyword && (
-        <div>
-          {keywordList &&
-            keywordList.map((keyword) => {
-              return <div key={keyword.key}>{keyword?.value}</div>;
-            })}
+        // keyword가 없을 때 최근 검색 내역 리스트 노출
+        <div className="flex-1 flex flex-col gap-4">
+          <div className="flex justify-between items-center">
+            <span className="font-semibold text-[#2D3035]">최근 검색 내역</span>
+            <button
+              className="text-[#9CA1AA] text-sm p-2"
+              onClick={() => deleteRecentKeywordList('ALL')}
+            >
+              전체삭제
+            </button>
+          </div>
+          {keywordList?.length > 0 ? (
+            <ul className="flex-1 flex flex-col">
+              {keywordList.map((keyword) => {
+                return (
+                  <li
+                    key={keyword.key}
+                    className="flex justify-between items-center border-b border-[#E9EBEC] py-3 px-2 gap-2"
+                  >
+                    <Link
+                      className="flex-grow"
+                      href={`/search?keyword=${keyword?.value}`}
+                    >
+                      <span className="text-[#2D3035] text-sm line-clamp-2">
+                        {keyword?.value}
+                      </span>
+                    </Link>
+                    <button
+                      onClick={(e) => deleteRecentKeywordList(keyword.key)}
+                    >
+                      <XMarkIcon className="w-6 h-6 text-gray-500 dark:text-gray-400 " />
+                    </button>
+                  </li>
+                );
+              })}
+            </ul>
+          ) : (
+            <Empty contents="최근 검색 내역이 없습니다." />
+          )}
         </div>
       )}
-      {searchList?.map((item) => {
-        return (
-          <Link
-            href={`/pet-tour/detail/${item?.contenttypeid}/${item?.contentid}`}
-            key={item?.contentid}
-            className="flex flex-col gap-2 basis-[calc(50%-calc(0.5rem/2))]"
-          >
-            <div className="relative after:content-[''] after:pb-[90%] after:block">
-              <Image
-                fill
-                className="object-cover rounded-lg box-border border border-[#F3F6F6]"
-                src={
-                  item?.firstimage ||
-                  item?.firstimage2 ||
-                  '/images/DefaultImage.webp'
-                }
-                sizes="100%" // sizes를 지정해달라고 해서 임시로 100%로 해놨는데 이거 무슨값을 넣어도 바뀌지않는데 왜그럴까?
-                alt={item?.title}
-              />
-            </div>
-            <div className="line-clamp-2">{item?.title}</div>
-          </Link>
-        );
-      })}
-      {listLoading &&
-        [1, 2, 3, 4].map((index) => {
-          return (
-            <Fragment key={index}>
-              <ImgSkeleton description />
-            </Fragment>
-          );
-        })}
-      {!listLoading && searchList?.length < pagination?.totalCount && (
-        <div ref={listLoadingRef.setRef}></div>
+
+      {query?.keyword && searchList.length == 0 ? (
+        // keyword가 있는데 검색 결과가 없을 때
+        <Empty contents="검색 결과가 없습니다." />
+      ) : (
+        <>
+          {searchList?.map((item) => {
+            return (
+              <Link
+                href={`/pet-tour/detail/${item?.contenttypeid}/${item?.contentid}`}
+                key={item?.contentid}
+                className="flex flex-col gap-2 basis-[calc(50%-calc(0.5rem/2))]"
+              >
+                <div className="relative after:content-[''] after:pb-[90%] after:block">
+                  <Image
+                    fill
+                    className="object-cover rounded-lg box-border border border-[#F3F6F6]"
+                    src={
+                      item?.firstimage ||
+                      item?.firstimage2 ||
+                      '/images/DefaultImage.webp'
+                    }
+                    sizes="100%" // sizes를 지정해달라고 해서 임시로 100%로 해놨는데 이거 무슨값을 넣어도 바뀌지않는데 왜그럴까?
+                    alt={item?.title}
+                  />
+                </div>
+                <div className="line-clamp-2">{item?.title}</div>
+              </Link>
+            );
+          })}
+          {listLoading &&
+            [1, 2, 3, 4].map((index) => {
+              return (
+                <Fragment key={index}>
+                  <ImgSkeleton description />
+                </Fragment>
+              );
+            })}
+          {!listLoading && searchList?.length < pagination?.totalCount && (
+            <div ref={listLoadingRef.setRef}></div>
+          )}
+        </>
       )}
     </div>
   );
