7cd8021c0b52024756daa0c25ff75c4da9c7a7d2
unknown <jh2000lee@nate.com>
2024-12-22 19:15:21 +0700

    [MOD]PetTourMap 적용

--- app/(none-footer-navigation)/pet-tour/detail/@modal/(.)map/page.jsx
+++ app/(none-footer-navigation)/pet-tour/detail/@modal/(.)map/page.jsx
@@ -1,94 +1,11 @@
-'use client';
-
-import { useState } from 'react';
-import { useSearchParams } from 'next/navigation';
-import { Map, ZoomControl, MapMarker } from 'react-kakao-maps-sdk';
-import { HomeIcon } from '@heroicons/react/24/outline';
 // COMPONENT
 import Modal from '@/components/common/Modal';
-// HOOK
-import useKakaoLoader from '@/hooks/useKakaoLoader';
+import PetTourMap from '@/app/(none-footer-navigation)/pet-tour/detail/map/PetTourMap';
 
-export default function DetailMappage({ params }) {
-  const searchParams = useSearchParams();
-  const mapx = searchParams.get('mapx') || ''; // mapy --> 위도(lat)
-  const mapy = searchParams.get('mapy') || ''; // mapx--> 경도(lng)
-  const title = searchParams.get('title') || ''; // 주소
-  const addr = searchParams.get('addr') || ''; // 주소
-
-  const [loading, error] = useKakaoLoader();
-
-  // 현재 지도 위치 --> 홈 버튼 누르면 초기로 이동 하기 위하여 관리
-  const [state, setState] = useState({
-    center: { lat: mapy, lng: mapx }, // 지도의 초기 위치
-    isPanto: false, // 지도 위치 변경시 panto를 이용할지에 대해서 정의
-  });
-
-  // 지도 드래그 이벤트
-  function onDragEnd(map) {
-    const latlng = map.getCenter();
-    setState({
-      center: { lat: latlng.getLat(), lng: latlng.getLng() },
-      isPanto: false,
-    });
-  }
-
-  // 주소 복사
-  async function copyToClipboard() {
-    try {
-      await navigator.clipboard.writeText(addr);
-      alert('클립보드에 복사되었습니다.');
-    } catch (error) {
-      console.error(error);
-    }
-  }
-
+export default function DetailMapModal({ params }) {
   return (
     <Modal title="지도" full bottomSlide>
-      {loading ? (
-        '로딩중.....'
-      ) : error ? (
-        '에러발생: Kakao Maps 로드 실패'
-      ) : (
-        <div className="w-full h-full">
-          <Map
-            center={state.center}
-            isPanto={state.isPanto}
-            style={{ width: '100%', height: '100%', position: 'relative' }}
-            level={3}
-            onDragEnd={onDragEnd}
-          >
-            <MapMarker position={{ lat: mapy, lng: mapx }} />
-            <ZoomControl position="RIGHT" />
-            <div className="fixed bottom-0 z-50 w-full">
-              <div className="flex justify-end p-4">
-                <button
-                  className=" bg-shadow-color rounded-full p-2.5"
-                  onClick={() => {
-                    setState({
-                      center: { lat: mapy, lng: mapx },
-                      isPanto: true,
-                    });
-                  }}
-                >
-                  <HomeIcon className="h-7 w-7 text-[#ffffff]" />
-                </button>
-              </div>
-              <div className="bg-[#ffffff] flex flex-col gap-1 p-4 shadow-[0px_0px_0.4rem_0px_rgba(0,0,0,0.3)]">
-                <span className="font-bold text-[#1A1A1A] text-base">
-                  {title}
-                </span>
-                <div className="text-xs flex justify-between">
-                  <span className="text-[#6D6D6D]">{addr}</span>
-                  <button className="text-[#0152CC]" onClick={copyToClipboard}>
-                    주소 복사
-                  </button>
-                </div>
-              </div>
-            </div>
-          </Map>
-        </div>
-      )}
+      <PetTourMap />
     </Modal>
   );
 }
--- app/(none-footer-navigation)/pet-tour/detail/map/page.jsx
+++ app/(none-footer-navigation)/pet-tour/detail/map/page.jsx
@@ -1,73 +1,9 @@
-import DOMPurify from 'dompurify';
-import Image from 'next/image';
-// ICON
-import {
-  PhoneIcon,
-  MapIcon,
-  ChevronRightIcon,
-  ChevronLeftIcon,
-} from '@heroicons/react/24/outline';
-// SERVICE
-import KorPetTourService from '@/service/KorPetTourService ';
-// UTIL
-import {
-  formatCommaNumber,
-  introCategorizedFields,
-  categories,
-} from '@/utils/format';
 // COMPONENT
-import ImgCarousel from '@/components/carousel/ImgCarousel';
-
+import PetTourMap from '@/app/(none-footer-navigation)/pet-tour/detail/map/PetTourMap';
 export default async function MapPage({ params }) {
-  const { contentInfo } = await params;
-  const contentTypeId = contentInfo?.[0];
-  const contentId = contentInfo?.[1];
-  console.log('여기는 페이지 이동!!!!!!!!!');
-  console.log('contentTypeId@@@@@@@@@@@@@@', contentTypeId);
-  console.log('contentId@@@@@@@@@@@@@@', contentId);
-
   return (
-    <div>
-      <div>여기는 지도 페이지</div>
-      <div>여기는 지도 페이지</div>
-      <div>여기는 지도 페이지</div>
-      <div>여기는 지도 페이지</div>
-      <div>여기는 지도 페이지</div>
-      <div>여기는 지도 페이지</div>
-      <div>여기는 지도 페이지</div>
-      <div>여기는 지도 페이지</div>
-      <div>여기는 지도 페이지</div>
-      <div>여기는 지도 페이지</div>
-      <div>여기는 지도 페이지</div>
-      <div>여기는 지도 페이지</div>
-      <div>여기는 지도 페이지</div>
-      <div>여기는 지도 페이지</div>
-      <div>여기는 지도 페이지</div>
-      <div>여기는 지도 페이지</div>
-      <div>여기는 지도 페이지</div>
-      <div>여기는 지도 페이지</div>
-      <div>여기는 지도 페이지</div>
-      <div>여기는 지도 페이지</div>
-      <div>여기는 지도 페이지</div>
-      <div>여기는 지도 페이지</div>
-      <div>여기는 지도 페이지</div>
-      <div>여기는 지도 페이지</div>
-      <div>여기는 지도 페이지</div>
-      <div>여기는 지도 페이지</div>
-      <div>여기는 지도 페이지</div>
-      <div>여기는 지도 페이지</div>
-      <div>여기는 지도 페이지</div>
-      <div>여기는 지도 페이지</div>
-      <div>여기는 지도 페이지</div>
-      <div>여기는 지도 페이지</div>
-      <div>여기는 지도 페이지</div>
-      <div>여기는 지도 페이지</div>
-      <div>여기는 지도 페이지</div>
-      <div>여기는 지도 페이지</div>
-      <div>여기는 지도 페이지</div>
-      <div>여기는 지도 페이지</div>
-      <div>여기는 지도 페이지</div>
-      <div>여기는 지도 페이지</div>
+    <div className="w-full h-screen">
+      <PetTourMap />
     </div>
   );
 }
