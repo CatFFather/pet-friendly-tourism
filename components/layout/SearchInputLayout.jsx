24ff4eb2eb66c0f3429e90974b35cb3542adcb60
이재홍 <jh2000lee@nate.com>
2024-12-30 18:19:40 +0900

    [MOD]MapPinIcon 추가 하여 지도에서 검색 할 수 있도록 할 예정

--- components/layout/SearchInputLayout.jsx
+++ components/layout/SearchInputLayout.jsx
@@ -1,7 +1,9 @@
 'use client';
 import Form from 'next/form';
+import Link from 'next/link';
 import { useSearchParams } from 'next/navigation';
-
+// ICON
+import { MapPinIcon } from '@heroicons/react/24/outline';
 // COMPONENT
 import SearchInput from '@/components/input/SearchInput';
 
@@ -11,7 +13,24 @@ export default function SearchInputLayout({}) {
   return (
     <div className="px-1.5 py-2">
       <Form key={keyword} action="">
-        <SearchInput name="keyword" defaultValue={keyword} />
+        <div className="flex gap-1 items-center">
+          <div className="flex-grow">
+            <SearchInput name="keyword" defaultValue={keyword} />
+          </div>
+          <Link href={{ pathname: '/pet-tour/detail/map' }} scroll={false}>
+            <MapPinIcon className="w-7 h-7 text-gray-500 dark:text-gray-400 " />
+          </Link>
+          {/* 
+          href={{
+              pathname: '/pet-tour/detail/map',
+              query: {
+                mapx: common?.mapx,
+                mapy: common?.mapy,
+                title: common?.title,
+                addr: common?.addr1 || common?.addr2,
+              },
+            }} */}
+        </div>
       </Form>
     </div>
   );
