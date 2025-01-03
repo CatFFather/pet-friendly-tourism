e11bc2a3c6e851ff9d15bb70c61624d4a9bbe89a
이재홍 <jh2000lee@nate.com>
2025-01-03 13:38:28 +0900

    [MOD]마커 클릭 시, 상세 모달 x 버튼 클릭 시 이벤트 추가 하여 슬라이드 애니메이션 구현

--- app/(none-footer-navigation)/pet-tour/search-map/SearchMap.jsx
+++ app/(none-footer-navigation)/pet-tour/search-map/SearchMap.jsx
@@ -1,7 +1,7 @@
 'use client';
 
 import _ from 'lodash';
-import { useEffect, useState, useCallback } from 'react';
+import { useEffect, useState, useCallback, useRef } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import { Map, ZoomControl, MapMarker } from 'react-kakao-maps-sdk';
@@ -24,7 +24,10 @@ export default function SearchMapPage({ params }) {
     isPanto: false, // 지도 위치 변경시 panto를 이용할지에 대해서 정의
   });
   const [locationBasedList, setLocationBasedList] = useState(null); // 위치기반 관광정보 리스트
+  const [listLoading, setListLoading] = useState(false);
   const [selectedMarkerInfo, setSelectedMarkerInfo] = useState(null); // 선택된 위치기반 관광정보 데이터
+  const selectedMarkerInfoWrapRef = useRef(null);
+  const bottomWrapRef = useRef(null);
 
   // 처음 내위치 저장
   useEffect(() => {
@@ -86,11 +89,29 @@ export default function SearchMapPage({ params }) {
 
   // 위치기반 관광정보조회
   function getLocationBasedList(params) {
+    setListLoading(true);
     KorPetTourService.getLocationBasedList(params)
       .then((res) =>
         setLocationBasedList(res?.data?.response?.body?.items?.item || []),
       )
-      .catch((e) => console.log('e', e));
+      .catch((e) => console.log('e', e))
+      .finally(() => setListLoading(false));
+  }
+
+  // 지도에 표기된 마커 클릭
+  function onClickMapMarker(info) {
+    bottomWrapRef?.current?.style?.setProperty('transform', 'translateY(0)');
+    selectedMarkerInfoWrapRef?.current?.style?.setProperty('opacity', 1);
+    setSelectedMarkerInfo(info);
+  }
+
+  // 클릭한 마커 정보 닫기
+  function onCloseSelectedMarkerInfo() {
+    bottomWrapRef?.current?.style?.setProperty('transform', 'translateY(100%)');
+    selectedMarkerInfoWrapRef?.current?.style?.setProperty('opacity', 0);
+    setTimeout(() => {
+      setSelectedMarkerInfo(null);
+    }, 300);
   }
 
   return (
@@ -100,7 +121,17 @@ export default function SearchMapPage({ params }) {
       ) : error ? (
         '에러발생: Kakao Maps 로드 실패'
       ) : (
-        <div className="w-full h-full">
+        <div className="relative w-full h-full overflow-hidden">
+          {/* <div className="absolute top-0 w-full z-50">
+            {listLoading == false && (
+              <div class="flex space-x-2 justify-center items-center dark:invert">
+                <span class="sr-only">Loading...</span>
+                <div class="h-8 w-8 bg-black rounded-full animate-bounce [animation-delay:-0.3s]"></div>
+                <div class="h-8 w-8 bg-black rounded-full animate-bounce [animation-delay:-0.15s]"></div>
+                <div class="h-8 w-8 bg-black rounded-full animate-bounce"></div>
+              </div>
+            )}
+          </div> */}
           <Map
             id="search_map_page"
             center={state.center}
@@ -124,94 +155,93 @@ export default function SearchMapPage({ params }) {
                   position={{ lat: info?.mapy, lng: info?.mapx }} // 마커를 표시할 위치
                   image={{
                     src: '/images/MapMarker.png', // 마커이미지의 주소입니다
-                    size: {
-                      width: 40,
-                      height: 49,
-                    }, // 마커이미지의 크기입니다
+                    size: { width: 40, height: 49 }, // 마커이미지의 크기입니다
+                    options: { alt: info?.title },
                   }}
                   title={info?.title} // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
-                  onClick={() => {
-                    setSelectedMarkerInfo(info);
-                  }}
+                  onClick={() => onClickMapMarker(info)}
                 />
               ))}
             <ZoomControl position="RIGHT" />
-            <div className="absolute bottom-0 z-50 w-full">
-              <div className="flex justify-end p-4">
-                <button
-                  className=" bg-shadow-color rounded-full p-2.5"
-                  onClick={() => {
-                    setState({
-                      center: {
-                        lat: currentLocation?.latitude,
-                        lng: currentLocation?.longitude,
-                      },
-                      isPanto: true,
-                    });
-                  }}
+          </Map>
+          <div
+            ref={bottomWrapRef}
+            className="absolute bottom-0 left-0 z-50 w-full transition-all duration-300 translate-y-full"
+          >
+            <div className="absolute -top-16 right-4 z-50">
+              <button
+                className=" bg-shadow-color rounded-full p-2.5"
+                onClick={() => {
+                  setState({
+                    center: {
+                      lat: currentLocation?.latitude,
+                      lng: currentLocation?.longitude,
+                    },
+                    isPanto: true,
+                  });
+                }}
+              >
+                <HomeIcon className="h-7 w-7 text-[#ffffff]" />
+              </button>
+            </div>
+            <div
+              ref={selectedMarkerInfoWrapRef}
+              className="relative bg-[#ffffff] flex flex-col gap-1 p-4 shadow-[0px_0px_0.4rem_0px_rgba(0,0,0,0.3)] transition-all duration-300 opacity-0"
+            >
+              <button
+                onClick={onCloseSelectedMarkerInfo}
+                className="hover:text-gray-700 p-2 absolute -top-2 right-0"
+              >
+                <XMarkIcon className="w-6 h-6 text-gray-500 dark:text-gray-400" />
+              </button>
+              <div className="flex gap-2">
+                <Link
+                  className="basis-2/4 flex-grow flex-shrink relative after:content-[''] after:pb-[90%] after:block"
+                  href={`/pet-tour/detail/${selectedMarkerInfo?.contenttypeid}/${selectedMarkerInfo?.contentid}`}
                 >
-                  <HomeIcon className="h-7 w-7 text-[#ffffff]" />
-                </button>
-              </div>
-              {selectedMarkerInfo && (
-                <div className="relative bg-[#ffffff] flex flex-col gap-1 p-4 shadow-[0px_0px_0.4rem_0px_rgba(0,0,0,0.3)]">
-                  <button
-                    onClick={() => setSelectedMarkerInfo(null)}
-                    className="hover:text-gray-700 p-2 absolute -top-1 right-0"
-                  >
-                    <XMarkIcon className="w-5 h-5 text-gray-500 dark:text-gray-400" />
-                  </button>
-                  <div className="flex gap-2">
-                    <Link
-                      className="basis-2/4 flex-grow flex-shrink relative after:content-[''] after:pb-[90%] after:block"
-                      href={`/pet-tour/detail/${selectedMarkerInfo?.contenttypeid}/${selectedMarkerInfo?.contentid}`}
-                    >
-                      <Image
-                        draggable={false}
-                        fill
-                        className="object-cover rounded-lg box-border border border-[#F3F6F6]"
-                        src={
-                          selectedMarkerInfo?.firstimage ||
-                          selectedMarkerInfo?.firstimage2 ||
-                          '/images/DefaultImage.webp'
-                        }
-                        sizes="100%" // sizes를 지정해달라고 해서 임시로 100%로 해놨는데 이거 무슨값을 넣어도 바뀌지않는데 왜그럴까?
-                        alt={selectedMarkerInfo?.title}
-                      />
-                    </Link>
+                  <Image
+                    draggable={false}
+                    fill
+                    className="object-cover rounded-lg box-border border border-[#F3F6F6]"
+                    src={
+                      selectedMarkerInfo?.firstimage ||
+                      selectedMarkerInfo?.firstimage2 ||
+                      '/images/DefaultImage.webp'
+                    }
+                    sizes="100%" // sizes를 지정해달라고 해서 임시로 100%로 해놨는데 이거 무슨값을 넣어도 바뀌지않는데 왜그럴까?
+                    alt={selectedMarkerInfo?.title || '기본 사진'}
+                  />
+                </Link>
 
-                    <div className="basis-2/4 flex flex-col">
-                      <Link
-                        className="mt-0.5"
-                        href={`/pet-tour/detail/${selectedMarkerInfo?.contenttypeid}/${selectedMarkerInfo?.contentid}`}
-                      >
-                        <span className="font-bold text-[#1A1A1A] text-base">
-                          {selectedMarkerInfo?.title}
-                        </span>
-                      </Link>
-                      <div className="text-xs flex-1 flex flex-col justify-between">
-                        <span className="text-[#6D6D6D]">
-                          {selectedMarkerInfo?.addr1 ||
-                            selectedMarkerInfo?.addr2}
-                        </span>
-                        <button
-                          className="text-[#0152CC] self-end"
-                          onClick={() =>
-                            copyToClipboard(
-                              selectedMarkerInfo?.addr1 ||
-                                selectedMarkerInfo?.addr2,
-                            )
-                          }
-                        >
-                          주소 복사
-                        </button>
-                      </div>
-                    </div>
+                <div className="basis-2/4 flex flex-col">
+                  <Link
+                    className="mt-1"
+                    href={`/pet-tour/detail/${selectedMarkerInfo?.contenttypeid}/${selectedMarkerInfo?.contentid}`}
+                  >
+                    <span className="font-bold text-[#1A1A1A] text-base">
+                      {selectedMarkerInfo?.title}
+                    </span>
+                  </Link>
+                  <div className="text-xs flex-1 flex flex-col justify-between">
+                    <span className="text-[#6D6D6D]">
+                      {selectedMarkerInfo?.addr1 || selectedMarkerInfo?.addr2}
+                    </span>
+                    <button
+                      className="text-[#0152CC] self-end"
+                      onClick={() =>
+                        copyToClipboard(
+                          selectedMarkerInfo?.addr1 ||
+                            selectedMarkerInfo?.addr2,
+                        )
+                      }
+                    >
+                      주소 복사
+                    </button>
                   </div>
                 </div>
-              )}
+              </div>
             </div>
-          </Map>
+          </div>
         </div>
       )}
     </>
